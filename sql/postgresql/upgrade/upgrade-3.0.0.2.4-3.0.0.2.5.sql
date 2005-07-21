f52f429034eb8a8d79c851c769a43fa695c9bb1d
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2005-07-21 18:32:57 +0000

    - fixed the project list pagination - changed the project list to display start_date and end_date   formatted - Added support for Unicode Trados 7.0 format to trados import

--- sql/postgresql/upgrade/upgrade-3.0.0.2.4-3.0.0.2.5.sql
+++ sql/postgresql/upgrade/upgrade-3.0.0.2.4-3.0.0.2.5.sql
@@ -24,3 +24,28 @@ ALTER TABLE im_projects DROP COLUMN start_date_new;
 COMMIT;
 
 
+
+-- use "formatted" start date in project views
+--
+delete from im_view_columns where column_id=2215;
+insert into im_view_columns (column_id, view_id, group_id, column_name, column_render_tcl,
+extra_select, extra_where, sort_order, visible_for) values (2215,22,NULL,'Start Date',
+'$start_date_formatted','','',15,'');
+
+delete from im_view_columns where column_id=2015;
+insert into im_view_columns (column_id, view_id, group_id, column_name, column_render_tcl,
+extra_select, extra_where, sort_order, visible_for) values (2015,20,NULL,'Start Date',
+'$start_date_formatted','','',8,'');
+
+
+-- use "formatted" end date in project views
+--
+delete from im_view_columns where column_id=2217;
+insert into im_view_columns (column_id, view_id, group_id, column_name, column_render_tcl,
+extra_select, extra_where, sort_order, visible_for) values (2217,22,NULL,'Delivery Date',
+'$end_date_formatted','','',16,'');
+
+delete from im_view_columns where column_id=2017;
+insert into im_view_columns (column_id, view_id, group_id, column_name, column_render_tcl,
+extra_select, extra_where, sort_order, visible_for) values (2017,20,NULL,'Delivery Date',
+'$end_date_formatted','','',9,'');

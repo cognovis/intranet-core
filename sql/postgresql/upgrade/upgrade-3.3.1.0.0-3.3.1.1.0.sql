d4a176bb9fbb6359d465b0343691a88cb0e60821
adverbage <>
2007-11-16 13:48:20 +0000

    - fixes to make upgrade 3.1.2 -> 3.3 or higher more smooth

--- sql/postgresql/upgrade/upgrade-3.3.1.0.0-3.3.1.1.0.sql
+++ sql/postgresql/upgrade/upgrade-3.3.1.0.0-3.3.1.1.0.sql
@@ -1,6 +1,105 @@
 -- upgrade-3.3.1.0.0-3.3.1.1.0.sql
 
 
+
+create or replace function im_component_plugin__new (
+	integer, varchar, timestamptz, integer, varchar, integer, 
+	varchar, varchar, varchar, varchar, varchar, integer, 
+	varchar, varchar
+) returns integer as '
+declare
+	p_plugin_id	alias for $1;	-- default null
+	p_object_type	alias for $2;	-- default ''acs_object''
+	p_creation_date	alias for $3;	-- default now()
+	p_creation_user	alias for $4;	-- default null
+	p_creation_ip	alias for $5;	-- default null
+	p_context_id	alias for $6;	-- default null
+
+	p_plugin_name	alias for $7;
+	p_package_name	alias for $8;
+	p_location	alias for $9;
+	p_page_url	alias for $10;
+	p_view_name	alias for $11;	-- default null
+	p_sort_order	alias for $12;
+	p_component_tcl	alias for $13;
+	p_title_tcl	alias for $14;
+
+	v_plugin_id	im_component_plugins.plugin_id%TYPE;
+begin
+	select plugin_id into v_plugin_id
+	from im_component_plugins
+	where plugin_name = p_plugin_name and package_name = p_package_name;
+	IF v_plugin_id is not null THEN return v_plugin_id; END IF;
+
+	v_plugin_id := acs_object__new (
+		p_plugin_id,	-- object_id
+		p_object_type,	-- object_type
+		p_creation_date,	-- creation_date
+		p_creation_user,	-- creation_user
+		p_creation_ip,	-- creation_ip
+		p_context_id	-- context_id
+	);
+
+	insert into im_component_plugins (
+		plugin_id, plugin_name, package_name, sort_order, 
+		view_name, page_url, location, 
+		component_tcl, title_tcl
+	) values (
+		v_plugin_id, p_plugin_name, p_package_name, p_sort_order, 
+		p_view_name, p_page_url, p_location, 
+		p_component_tcl, p_title_tcl
+	);
+
+	return v_plugin_id;
+end;' language 'plpgsql';
+
+
+create or replace function im_component_plugin__new (
+	integer, varchar, timestamptz, integer, varchar, integer, 
+	varchar, varchar, varchar, varchar, varchar, integer, 
+	varchar
+) returns integer as '
+declare
+	p_plugin_id	alias for $1;	-- default null
+	p_object_type	alias for $2;	-- default ''acs_object''
+	p_creation_date	alias for $3;	-- default now()
+	p_creation_user	alias for $4;	-- default null
+	p_creation_ip	alias for $5;	-- default null
+	p_context_id	alias for $6;	-- default null
+
+	p_plugin_name	alias for $7;
+	p_package_name	alias for $8;
+	p_location	alias for $9;
+	p_page_url	alias for $10;
+	p_view_name	alias for $11;	-- default null
+	p_sort_order	alias for $12;
+	p_component_tcl	alias for $13;
+
+	v_plugin_id	im_component_plugins.plugin_id%TYPE;
+begin
+	v_plugin_id := im_component_plugin__new (
+		p_plugin_id,
+		p_object_type,
+		p_creation_date,
+		p_creation_user,
+		p_creation_ip,
+		p_context_id,
+
+		p_plugin_name,
+		p_package_name,
+		p_location,
+		p_page_url,
+		p_view_name,
+		p_sort_order,
+		p_component_tcl,
+		null
+	);
+
+	return v_plugin_id;
+end;' language 'plpgsql';
+
+
+
 -- Fix issue with im_costs__name -> im_cost__name
 
 update acs_object_types set name_method = 'im_cost__name' where object_type = 'im_cost';

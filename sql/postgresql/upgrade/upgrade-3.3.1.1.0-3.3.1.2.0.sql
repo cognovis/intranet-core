ce9447516efbb1752e050a8decbbf1ad0ac9921b
lawtank <>
2007-11-28 22:56:08 +0000

    - Lawtank changes

--- sql/postgresql/upgrade/upgrade-3.3.1.1.0-3.3.1.2.0.sql
+++ sql/postgresql/upgrade/upgrade-3.3.1.1.0-3.3.1.2.0.sql
@@ -22,3 +22,44 @@ BEGIN
         RETURN 0;
 end;' language 'plpgsql';
 
+
+
+
+-- Add "require_manual_login" privilege
+
+create or replace function inline_0 ()
+returns integer as '
+declare
+        v_count                 integer;
+begin
+        select count(*) into v_count from acs_privileges
+        where privilege = ''require_manual_login'';
+        IF 0 != v_count THEN return 0; END IF;
+
+	PERFORM acs_privilege__create_privilege(
+		''require_manual_login'',
+		''Require manual login - dont allow auto-login'',
+		''Require manual login - dont allow auto-login''
+	);
+	PERFORM acs_privilege__add_child(''admin'', ''require_manual_login'');
+
+    return 0;
+end;' language 'plpgsql';
+select inline_0 ();
+drop function inline_0 ();
+
+select im_priv_create('require_manual_login','P/O Admins');
+select im_priv_create('require_manual_login','Senior Managers');
+select im_priv_create('require_manual_login','Project Managers');
+select im_priv_create('require_manual_login','Accounting');
+-- select im_priv_create('require_manual_login','Employees');
+
+
+-- Add a "rounding factor" to currencies.
+-- This factor is 100 (2 digits) for EUR, USD, GBP etc,
+-- but 20 for CHF (rounding on 0.05 CHF)
+alter table currency_codes
+add rounding_factor integer default 100;
+
+update currency_codes set rounding_factor = 20 where iso='CHF';
+

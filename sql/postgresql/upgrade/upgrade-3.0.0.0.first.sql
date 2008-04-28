34c125072c7650d92651f865b06ce9231491bfb7
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2008-04-28 18:18:57 +0000

    - Upgrade script update before V3.3 release

--- sql/postgresql/upgrade/upgrade-3.0.0.0.first.sql
+++ sql/postgresql/upgrade/upgrade-3.0.0.0.first.sql
@@ -384,9 +384,120 @@ end;' language 'plpgsql';
 -- 
 -----------------------------------------------------------------------
 
+-- Duplicate tollerant version of create_type
+create or replace function acs_object_type__create_type (
+	varchar, varchar, varchar, varchar, varchar,
+	varchar, varchar, boolean, varchar, varchar
+) returns integer as '
+declare
+	create_type__object_type		alias for $1;	
+	create_type__pretty_name		alias for $2;	
+	create_type__pretty_plural		alias for $3;	
+	create_type__supertype			alias for $4;	
+	create_type__table_name			alias for $5;	
+	create_type__id_column			alias for $6;	-- default ''XXX''
+	create_type__package_name		alias for $7;	-- default null
+	create_type__abstract_p			alias for $8;	-- default ''f''
+	create_type__type_extension_table	alias for $9;	-- default null
+	create_type__name_method		alias for $10;	-- default null
+	v_package_name				acs_object_types.package_name%TYPE;
+	v_supertype				acs_object_types.supertype%TYPE;
+	v_name_method				varchar;
+	v_idx					integer;
+	v_count					integer;
+begin
+	select count(*) into v_count from acs_object_types
+	where object_type = create_type__object_type;
+	if v_count > 0 then return 0; end if;
+
+	v_idx := position(''.'' in create_type__name_method);
+	if v_idx <> 0 then
+		 v_name_method := substr(create_type__name_method,1,v_idx - 1) || 
+			 ''__'' || substr(create_type__name_method, v_idx + 1);
+	else 	 v_name_method := create_type__name_method;
+	end if;
+
+	if create_type__package_name is null or create_type__package_name = '''' then
+		v_package_name := create_type__object_type;
+	else	v_package_name := create_type__package_name;
+	end if;
+
+	if create_type__supertype is null or create_type__supertype = '''' then
+		v_supertype := ''acs_object'';
+	else	v_supertype := create_type__supertype;
+	end if;
+
+	insert into acs_object_types (
+		object_type, pretty_name, pretty_plural, supertype, table_name,
+		 id_column, abstract_p, type_extension_table, package_name,
+		 name_method
+	) values (
+		create_type__object_type, create_type__pretty_name, 
+		 create_type__pretty_plural, v_supertype, 
+		 create_type__table_name, create_type__id_column, 
+		 create_type__abstract_p, create_type__type_extension_table, 
+		 v_package_name, v_name_method
+	);
+
+	return 0; 
+end;' language 'plpgsql';
+
+
+
 -----------------------------------------------------------------------
 -- 
 -----------------------------------------------------------------------
+
+create or replace function im_report__new (
+	integer, varchar, timestamptz, integer, varchar, integer,
+	varchar, varchar, integer, integer, integer, text
+) returns integer as '
+DECLARE
+	p_report_id		alias for $1;		-- report_id  default null
+	p_object_type   	alias for $2;		-- object_type default ''im_report''
+	p_creation_date 	alias for $3;		-- creation_date default now()
+	p_creation_user 	alias for $4;		-- creation_user default null
+	p_creation_ip   	alias for $5;		-- creation_ip default null
+	p_context_id		alias for $6;		-- context_id default null
+
+	p_report_name		alias for $7;		-- report_name
+	p_report_code		alias for $8;
+	p_report_type_id	alias for $9;		
+	p_report_status_id	alias for $10;
+	p_report_menu_id	alias for $11;
+	p_report_sql		alias for $12;
+
+	v_report_id		integer;
+	v_count			integer;
+BEGIN
+	select count(*) into v_count from im_reports
+	where report_name = p_report_name;
+	if v_count > 0 then return 0; end if;
+
+	v_report_id := acs_object__new (
+		p_report_id,		-- object_id
+		p_object_type,		-- object_type
+		p_creation_date,	-- creation_date
+		p_creation_user,	-- creation_user
+		p_creation_ip,		-- creation_ip
+		p_context_id,		-- context_id
+		''t''			-- security_inherit_p
+	);
+
+	insert into im_reports (
+		report_id, report_name, report_code,
+		report_type_id, report_status_id,
+		report_menu_id, report_sql
+	) values (
+		v_report_id, p_report_name, p_report_code,
+		p_report_type_id, p_report_status_id,
+		p_report_menu_id, p_report_sql
+	);
+
+	return v_report_id;
+END;' language 'plpgsql';
+
+
 
 -----------------------------------------------------------------------
 -- 

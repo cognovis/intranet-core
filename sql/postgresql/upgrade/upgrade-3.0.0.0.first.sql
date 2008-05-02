00dc73f0ef92a64b2afa3432ebacd7a7ca194d3e
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2008-05-02 11:27:59 +0000

    - fixing update error PO-3.1.4 - 3.3 on Windows Installer

--- sql/postgresql/upgrade/upgrade-3.0.0.0.first.sql
+++ sql/postgresql/upgrade/upgrade-3.0.0.0.first.sql
@@ -180,6 +180,24 @@ end;' language 'plpgsql';
 -- im_plugin_components
 -----------------------------------------------------------------------
 
+-- Add a "title_tcl" field to Components
+--
+create or replace function inline_0 ()
+returns integer as '
+declare
+	v_count	 integer;
+begin
+	select count(*) into v_count from user_tab_columns
+	where table_name = ''IM_COMPONENT_PLUGINS'' and column_name = ''TITLE_TCL'';
+	if v_count > 0 then return 0; end if;
+
+	alter table im_component_plugins add title_tcl text;
+
+	return 0;
+end;' language 'plpgsql';
+select inline_0 ();
+drop function inline_0 ();
+
 
 create or replace function im_component_plugin__new (
 	integer, varchar, timestamptz, integer, varchar, integer, 

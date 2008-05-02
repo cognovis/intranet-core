2670395e9a031e28ca3b1b9225cec35bf9653163
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2008-05-02 11:18:01 +0000

    - added day_enumerator to first-load procedures

--- sql/postgresql/upgrade/upgrade-3.0.0.0.first.sql
+++ sql/postgresql/upgrade/upgrade-3.0.0.0.first.sql
@@ -638,6 +638,22 @@ end;' language 'plpgsql';
 -- 
 -----------------------------------------------------------------------
 
+create or replace function im_day_enumerator (
+	date, date
+) returns setof date as '
+declare
+	p_start_date		alias for $1;
+	p_end_date		alias for $2;
+	v_date			date;
+BEGIN
+	v_date := p_start_date;
+	WHILE (v_date < p_end_date) LOOP
+		RETURN NEXT v_date;
+		v_date := v_date + 1;
+	END LOOP;
+	RETURN;
+end;' language 'plpgsql';
+
 
 -----------------------------------------------------------------------
 -- 

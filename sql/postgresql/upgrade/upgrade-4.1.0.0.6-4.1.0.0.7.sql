b9830d27f55208af61fbd0bacb3d5c5c1ca83062
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2014-10-27 13:14:09 +0000

    Summary: - Fixed translation issue

--- sql/postgresql/upgrade/upgrade-4.1.0.0.6-4.1.0.0.7.sql
+++ sql/postgresql/upgrade/upgrade-4.1.0.0.6-4.1.0.0.7.sql
@@ -8,3 +8,15 @@ update lang_messages set message = 'MONTH DD YYYY' where locale = 'nl_NL' and me
 update lang_messages set message = 'MONTH DD YYYY' where locale = 'no_NO' and message_key = 'localization-formbuilder_date_format';
 update lang_messages set message = 'DD MONTH YYYY' where locale = 'tr_TR' and message_key = 'localization-formbuilder_date_format';
 
+
+-- Fix SQL Metadata for im_company_employee_rel
+-- This fixes a bug in the REST interface
+update acs_object_type_tables
+set table_name = 'im_company_employee_rels'
+where table_name = 'im_company_employee_rel';
+
+-- Update a messae translation with unquoted HTML characters
+update lang_messages
+set message = 'C&amp;KM Help'
+where message = 'C&KM Help';
+
